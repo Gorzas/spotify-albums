7a1486f17aac4fa7818c407351051dfd040219c8
Gorzas <joseda87@gmail.com>
2018-01-22 19:56:07 +0100

    Add function get albums from CSV

--- node/get_albums.js
+++ node/get_albums.js
@@ -1,16 +1,16 @@
 /**
  * Get Spotify albums links
  * --------------------------------------------------
- * 
+ *
  * Script for Node based in the examples provided for Spotify.
- * 
- * - Spotify original examples: 
+ *
+ * - Spotify original examples:
  * https://github.com/spotify/web-api-auth-examples/
  *
  *
  * Usage:
  *   node get_albums.js
- * 
+ *
  **/
 // Variables
 const csv = require('csv');
@@ -18,21 +18,22 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const fs = require('fs');
 const config = require('./config.json');
 let accessToken;
+let credentials;
 let spotifyApi;
 /**
  * exportAlbums
  * ----------------------
- * 
+ *
  * Receives a JSON Object and exports it in a text file.
  *
  * The text file is an ordered list in markdown with a list of the total of
- * artists of the playlist. 
- * 
+ * artists of the playlist.
+ *
  * This is an example of retrieving data with the Spotify API and you can
  * modify this code as you like.
- * 
+ *
  * @param  {Object} json JSON response of the Spotify API
- * 
+ *
  * @return {Boolean}     True if everything went OK
  */
 function exportAlbums(json) {
@@ -69,29 +70,24 @@ spotifyApi = new SpotifyWebApi({
 });
 
 spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    console.log('The access token expires in ' + data.body['expires_in']);
-    console.log('The access token is ' + data.body['access_token']);
+  .then(
+    async (cred) => {
+      let data;
+      spotifyApi.setAccessToken(cred.body['access_token']);
 
-    // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body['access_token']);
+      try {
+        data = await spotifyApi.searchAlbums(`artist:The Bloody Beetroots album:The Great Electronic Swindle`, { limit: 1 });
 
-    spotifyApi.searchAlbums(`artist:The Bloody Beetroots album:The Great Electronic Swindle`, { limit: 1 })
-      .then(data => console.log(data.body))
-      .catch(data => console.log(data))
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err.message);
-  });
+        console.log(data.body.albums.items[0].external_urls.spotify);
+      } catch (e) {
+        console.log(e);
 
-// try {
-//   let data = await spotifyApi.clientCredentialsGrant();
+        return false;
+      }
 
-//   spotifyApi.setAccessToken(data.body['access_token']);
-// } catch (e) {
-//   console.log(e);
 
-//   return false;
-// }
+    }
+  );
 
 // spotifyApi.searchAlbums(`artist:The Bloody Beetroots album:The Great Electronic Swindle`, { limit: 1 })
 //   .then(data => console.log(data))
@@ -101,7 +97,7 @@ spotifyApi.clientCredentialsGrant()
 // fs.createReadStream('albums.csv')
 //   .pipe(csv.parse())
 //   .on(
-//     'data', 
+//     'data',
 //     (data) => {
 //       if (data.length) {
 //         Promise.all(
