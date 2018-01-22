b254b0dc5318026caed77be8d97d7134fbff0d5b
Gorzas <joseda87@gmail.com>
2018-01-22 19:56:52 +0100

    Add parse CSV function

---
+++ src/utils/parse-csv.js
@@ -0,0 +1,26 @@
+const csv = require('csv');
+const fs = require('fs');
+
+function parseCSV(dir, columns = []) {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(dir)
+      .pipe(csv.parse(
+        (err, data) => {
+          if (data.length) {
+            resolve(
+              data.map(values => {
+                return columns.reduce(
+                  (prev, col, i) => Object.assign({}, prev, { [col]: values[i] }),
+                  {},
+                );
+              })
+            );
+          } else {
+            reject();
+          }
+        },
+      ));
+  });
+}
+
+module.exports = parseCSV;
